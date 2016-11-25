5b4af3628be1d0409ff65bbc7925c505e7ba1d8b
Claude Pageau <pageauc@gmail.com>
2016-11-25 07:28:33 -0600

    version 0.f0 updated code comments

--- face-track.py
+++ face-track.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
-progname = "motion_track.py"
-ver = "version 0.96"
+progname = "face_track.py"
+ver = "version 0.50"
 
 """
-motion-track ver 0.95 written by Claude Pageau pageauc@gmail.com
-Raspberry (Pi) - python opencv2 motion tracking using picamera module
+motion-track ver 0.50 is written by Claude Pageau pageauc@gmail.com
+Raspberry (Pi) - python opencv2 motion and face tracking using picamera module
+attached to an openelectrons pan/tilt assembly http://www.mindsensors.com/rpi/33-pi-pan
 
-This is a raspberry pi python opencv2 motion tracking demonstration program.
-It will detect motion in the field of view and use opencv to calculate the
-largest contour and return its x,y coordinate.  I will be using this for
-a simple RPI robotics project, but thought the code would be useful for 
-other users as a starting point for a project.  I did quite a bit of 
-searching on the internet, github, etc but could not find a similar
-implementation that returns x,y coordinates of the most dominate moving 
-object in the frame.  Some of this code is base on a YouTube tutorial by
+This is a raspberry pi python opencv2 motion and face tracking demonstration program.
+It will detect motion or face in the field of view and use opencv to calculate the
+largest contour or position of face and return its x,y coordinate.
+It will then track using pan/tilt to keep the object/face in view. 
+Some of this code is base on a YouTube tutorial by
 Kyle Hounslow using C here https://www.youtube.com/watch?v=X6rPdRZzgjg
 
-Here is a my YouTube video demonstrating this demo program using a 
-Raspberry Pi B2 https://youtu.be/09JS7twPBsQ
+Here is a my YouTube video demonstrating a similar motion tracking only
+program using a Raspberry Pi B2 https://youtu.be/09JS7twPBsQ.  face-track 
+is based on the motion-track code
 
 Requires a Raspberry Pi with a RPI camera module installed and configured
-dependencies. Cut and paste command below into a terminal sesssion to
+. Cut and paste command below into a terminal sesssion to
 download and install motion_track demo.  Program will be installed to
 ~/motion-track-demo folder
 
-curl -L https://raw.github.com/pageauc/motion-track/master/motion-track-install.sh | bash
+curl -L https://raw.github.com/pageauc/face-track-demo/master/face-track-install.sh | bash
 
-To Run Demo
+To Run Demo (Note a reboot may be required depending on the number of updates.
 
-cd ~/motion-track-demo
-./motion-track.py
+cd ~/face-track-demo
+./face-track.py
 
 """
 print("%s %s using python2 and OpenCV2" % (progname, ver))
